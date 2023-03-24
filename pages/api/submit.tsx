3f1efcbdd5f17b8d8a59c333e5ca46ebbfc71fe9
Nitya <nityanalamari@gmail.com>
2023-03-24 06:58:02 -0400

    removed unwanted files

--- pages/api/submit.tsx
+++ pages/api/submit.tsx
@@ -1,6 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default (request: NextApiRequest, res: NextApiResponse) => {
-    console.log(request);
   res.status(200).json({ text: request.body })
 }
